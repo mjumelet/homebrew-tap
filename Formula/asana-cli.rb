7193a12f42b04835aac85a00eba7a8a6968e1ba1
Maurice Jumelet <m.jumelet@dutchview.com>
2026-02-17 10:00:01 +0100

    Update asana-cli to v1.0.0

--- Formula/asana-cli.rb
+++ Formula/asana-cli.rb
@@ -2,25 +2,25 @@ class AsanaCli < Formula
   desc "A fast, lightweight command-line interface for Asana"
   homepage "https://github.com/mjumelet/asana-cli"
   license "MIT"
-  version "0.1.0"
+  version "1.0.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mjumelet/asana-cli/releases/download/v#{version}/asana-cli_#{version}_darwin_arm64.tar.gz"
-      sha256 "PLACEHOLDER_ARM64_SHA256"
+      sha256 "8ccfad4e45b528c85f32a55d088f70c135e61ae94d10ffef49cef3e7f4641691"
     else
       url "https://github.com/mjumelet/asana-cli/releases/download/v#{version}/asana-cli_#{version}_darwin_amd64.tar.gz"
-      sha256 "PLACEHOLDER_AMD64_SHA256"
+      sha256 "8e6676d87d8107c7d6c93fff14be6a720d98d2a22c072ac6b943966122ef7ee9"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/mjumelet/asana-cli/releases/download/v#{version}/asana-cli_#{version}_linux_arm64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
+      sha256 "4f1d02e21a3c10ee1d97ae5a50525b0e8321c298a536631c52e814d0c0314bf3"
     else
       url "https://github.com/mjumelet/asana-cli/releases/download/v#{version}/asana-cli_#{version}_linux_amd64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_AMD64_SHA256"
+      sha256 "2005fbe68857525ea32d4a4da2410556576ccad040730a39947117d61bcae09c"
     end
   end
 
